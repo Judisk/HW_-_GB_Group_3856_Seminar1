b1194d1779332ce2b5ad343d5f64dcaedc6ff64a
Judisk <122523925+Judisk@users.noreply.github.com>
2023-01-26 22:57:23 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Start
+// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("Задача 2");
+
+System.Console.WriteLine("Введите число 1");
+
+int NumberOneT2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число 2");
+
+int NumberTwoT2 = Convert.ToInt32(Console.ReadLine());
+
+if (NumberOneT2>NumberTwoT2)
+{
+    System.Console.WriteLine("a  = "+ NumberOneT2 + "b = "+ NumberTwoT2+" -> Max = "+ NumberOneT2 );
+    }
+else
+{
+    System.Console.WriteLine("a  = "+ NumberOneT2 + " b = "+ NumberTwoT2+" -> Max = "+ NumberTwoT2 );
+    }
+
+
+
+
+
+
+
